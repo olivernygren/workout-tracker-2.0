c07e078f9e24bd4cb86ab6f65795a2263ab29802
Oliver Nygren <oliver.nygren@icloud.com>
2022-02-02 10:11:47 +0100

    get workouts from db, wrong order

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import { ThemeProvider } from '@material-ui/core';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
-import { db } from './firebase-config';
-import { collection, getDocs } from 'firebase/firestore';
 import { Header } from './components';
 import { routes } from './routes';
 import theme from './theme';
-import { useEffect } from 'react';
 
 function App() {
-	const workoutsCollection = collection(db, 'workouts');
-
-	useEffect(() => {
-		const getWorkouts = async () => {
-			const data = await getDocs(workoutsCollection);
-			console.log(data);
-		};
-		getWorkouts();
-	}, []);
-
 	return (
 		<ThemeProvider theme={theme}>
 			<BrowserRouter>
--- src/pages/allWorkoutsPage/index.tsx
+++ src/pages/allWorkoutsPage/index.tsx
@@ -1,15 +1,36 @@
-import { Grid, Button } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import { Grid, Button, CircularProgress } from '@material-ui/core';
 import { AddRounded } from '@material-ui/icons';
 import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 
+import { db } from '../../firebase-config';
 import useStyles from './styles';
 import { Page, TitleHeader, WorkoutCard } from '../../components';
 import { workoutNameToPathConverter, workouts } from '../../utils';
 import { getNumberOfSets } from '../../utils/getNumberOfSets';
+import { Workout } from '../../types';
 
 export const AllWorkoutsPage = () => {
 	const classes = useStyles();
 	const navigateTo = useNavigate();
+
+	const workoutsCollectionRef = collection(db, 'workouts');
+	const [workoutsFromDB, setWorkoutsFromDB] = useState<any[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		// setIsLoading(true);
+		const getWorkouts = async () => {
+			const data = await getDocs(workoutsCollectionRef);
+			setWorkoutsFromDB(
+				data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+				// data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+			);
+		};
+		getWorkouts();
+		setIsLoading(false);
+	}, []);
 
 	const HeaderButton = () => (
 		<Button
@@ -30,29 +51,56 @@ export const AllWorkoutsPage = () => {
 
 	return (
 		<Page title="Alla Pass">
-			<Grid
-				item
-				container
-				direction="column"
-				className={classes.titleHeaderContainer}
-			>
-				<TitleHeader
-					title="Alla pass"
-					navigateBackButton
-					navigateTo="/"
-					button={<HeaderButton />}
-				/>
-			</Grid>
-			<Grid item container direction="column" className={classes.cardContainer}>
-				{workouts.map((workout) => (
+			{isLoading ? (
+				<Grid
+					item
+					container
+					style={{ alignItems: 'center', justifyContent: 'center' }}
+				>
+					<CircularProgress />
+				</Grid>
+			) : (
+				<>
+					<Grid
+						item
+						container
+						direction="column"
+						className={classes.titleHeaderContainer}
+					>
+						<TitleHeader
+							title="Alla pass"
+							navigateBackButton
+							navigateTo="/"
+							button={<HeaderButton />}
+						/>
+					</Grid>
+					<Grid
+						item
+						container
+						direction="column"
+						className={classes.cardContainer}
+					>
+						{/* {workouts.map((workout) => (
 					<WorkoutCard
 						name={workout.name}
 						sets={getNumberOfSets(workout)}
 						path={workoutNameToPathConverter(workout.name)}
 						key={workout.name}
 					/>
-				))}
-			</Grid>
+				))} */}
+						{workoutsFromDB.map((workout) => {
+							return (
+								<WorkoutCard
+									name={workout.name}
+									sets={getNumberOfSets(workout)}
+									path={workoutNameToPathConverter(workout.name)}
+									key={workout.id}
+								/>
+							);
+						})}
+					</Grid>
+				</>
+			)}
 		</Page>
 	);
 };
