7b853d2ab7d1ef3a1c197f177aa9ad5754718d90
Oliver Nygren <oliver.nygren@icloud.com>
2022-11-07 15:27:01 +0100

    first version of exercise menu

--- src/components/exercise-card/ExerciseCard.tsx
+++ src/components/exercise-card/ExerciseCard.tsx
@@ -1,42 +1,111 @@
-import { Box, Grid, IconButton, Typography } from '@material-ui/core';
-import { ReplayRounded } from '@material-ui/icons';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  Popover,
+  Typography,
+} from '@material-ui/core';
+import {
+  DeleteRounded,
+  EditRounded,
+  MoreVert,
+  ReplayRounded,
+} from '@material-ui/icons';
 import { useNavigate } from 'react-router-dom';
-import { exerciseNameToPathConverter, ProgressIcon } from '../../utils';
 
+import { exerciseNameToPathConverter, ProgressIcon } from '../../utils';
 import useStyles from './styles';
 
 interface IExerciseCard {
-	exercise: string;
-	sets: number;
-	repRange: string;
+  exercise: string;
+  sets: number;
+  repRange: string;
 }
 
 export const ExerciseCard = ({ exercise, sets, repRange }: IExerciseCard) => {
-	const classes = useStyles();
-	const navigateTo = useNavigate();
-	const setsAndRepsString = `${sets} × ${repRange}`;
+  const classes = useStyles();
+  const navigateTo = useNavigate();
+  const setsXReps = `${sets} × ${repRange}`;
 
-	const handleNavigate = () => {
-		navigateTo(`/progress/${exerciseNameToPathConverter(exercise)}`);
-	};
+  const [showMenu, setShowMenu] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
 
-	return (
-		<Box className={classes.container}>
-			<Grid item container direction="column">
-				<Typography variant="subtitle1">{exercise}</Typography>
-				<Grid item container className={classes.setsXrepsContainer}>
-					<ReplayRounded className={classes.icon} fontSize="small" />
-					<Typography variant="body1" className={classes.setsXreps}>
-						{setsAndRepsString}
-					</Typography>
-				</Grid>
-			</Grid>
-			<IconButton className={classes.progressButton} onClick={handleNavigate}>
-				<ProgressIcon />
-			</IconButton>
-		</Box>
-	);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    setShowMenu(!showMenu);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setShowMenu(false);
+  };
+
+  const handleNavigate = () => {
+    navigateTo(`/progress/${exerciseNameToPathConverter(exercise)}`);
+  };
+
+  const ExerciseMenu = () => (
+    <Popover
+      open={showMenu}
+      onClose={handleClose}
+      anchorEl={anchorEl}
+      PaperProps={{ className: classes.popover }}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+    >
+      <Box className={classes.popoverItem} onClick={handleNavigate}>
+        <Typography variant="body1">Progress</Typography>
+        <ProgressIcon />
+      </Box>
+      <Divider />
+      <Box className={classes.popoverItem}>
+        <Typography variant="body1" className={classes.disabled}>
+          Ändra
+        </Typography>
+        <EditRounded className={classes.disabledIcon} fontSize="small" />
+      </Box>
+      <Divider />
+      <Box className={classes.popoverItem}>
+        <Typography variant="body1" className={classes.disabled}>
+          Ta bort
+        </Typography>
+        <DeleteRounded className={classes.disabledIcon} fontSize="small" />
+      </Box>
+    </Popover>
+  );
+
+  return (
+    <Box className={classes.container}>
+      <Grid item container direction="column">
+        <Typography variant="subtitle1">{exercise}</Typography>
+        <Grid item container className={classes.setsXrepsContainer}>
+          <ReplayRounded className={classes.icon} fontSize="small" />
+          <Typography variant="body1" className={classes.setsXreps}>
+            {setsXReps}
+          </Typography>
+        </Grid>
+      </Grid>
+      <IconButton
+        className={classes.progressButton}
+        onClick={handleClick}
+        disableRipple
+      >
+        {/* <ProgressIcon /> */}
+        <MoreVert className={classes.menuIcon} />
+      </IconButton>
+      <ExerciseMenu />
+    </Box>
+  );
 };
 
 export default ExerciseCard;
--- src/components/exercise-card/styles.ts
+++ src/components/exercise-card/styles.ts
@@ -1,32 +1,54 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-	container: {
-		backgroundImage: theme.palette.gradient.gradient,
-		borderRadius: 12,
-		marginBottom: 16,
-		padding: '8px 13px',
-		display: 'flex',
-		alignItems: 'center',
-	},
-	progressButton: {
-		backgroundColor: theme.palette.grey[800],
-		height: 'fit-content',
-		borderRadius: 10,
-		'&:hover': {
-			borderRadius: 10,
-			backgroundColor: theme.palette.grey[900],
-		},
-	},
-	setsXrepsContainer: {
-		alignItems: 'center',
-	},
-	setsXreps: {
-		color: theme.palette.grey[400],
-	},
-	icon: {
-		color: theme.palette.primary.light,
-		marginLeft: -3,
-		marginRight: 6,
-	},
+  container: {
+    backgroundImage: theme.palette.gradient.gradient,
+    borderRadius: 12,
+    marginBottom: 16,
+    padding: '8px 13px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  progressButton: {
+    // backgroundColor: theme.palette.grey[800],
+    padding: 4,
+    height: 'fit-content',
+    borderRadius: 8,
+    '&:hover': {
+      borderRadius: 8,
+      backgroundColor: theme.palette.grey[900],
+    },
+  },
+  setsXrepsContainer: {
+    alignItems: 'center',
+  },
+  setsXreps: {
+    color: theme.palette.grey[400],
+  },
+  icon: {
+    color: theme.palette.primary.light,
+    marginLeft: -3,
+    marginRight: 6,
+  },
+  menuIcon: {
+    color: theme.palette.common.white,
+  },
+  disabledIcon: {
+    color: theme.palette.grey[500],
+  },
+  popover: {
+    padding: '2px 14px',
+    backgroundColor: theme.palette.grey[800],
+    borderRadius: 10,
+    width: 180,
+  },
+  popoverItem: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 0',
+  },
+  disabled: {
+    color: theme.palette.grey[500],
+  },
 }));
